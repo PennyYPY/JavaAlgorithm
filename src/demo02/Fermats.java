b04169ba13d439cfb373fb4f6c918c7de76e5d22
PennyYPY <749098714@qq.com>
2018-03-28 16:25:49 +0800

    2018.3.28

---
+++ src/demo02/Fermats.java
@@ -0,0 +1,17 @@
+package demo02;
+
+/**
+ * Created by Administrator on 2018/3/27.
+ */
+public class Fermats {
+
+    public static boolean judge(int a){
+
+        int n = a;
+
+
+
+        return true;
+    }
+
+}
